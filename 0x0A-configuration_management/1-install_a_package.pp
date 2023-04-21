31213763731e3ace465c43bb1f69a99804d16d74
Frankenstei <enwereloveday@yahoo.com>
2023-04-21 15:18:37 -0700

    all

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,6 @@
+# installs puppet-lint
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# kills a process named killmenow
+
+exec { 'process kill killmenow':
+  path    => '/usr/bin/',
+  command => 'pkill -f killmenow',
+}
